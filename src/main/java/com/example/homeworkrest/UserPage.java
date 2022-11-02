40a2914da6422c4120f59e0bcfa7fce7875cb6ee
PKKostov18 <PKKostov18@codingburgas.bg>
2022-11-02 14:45:04 +0200

    Adding new lines in user page

--- src/main/java/com/example/homeworkrest/UserPage.java
+++ src/main/java/com/example/homeworkrest/UserPage.java
@@ -8,6 +8,6 @@ public class UserPage {
 
     @RequestMapping("/user")
     public String printUserPageText() {
-        return "User: Plamen Kostov" + " Age: 18" + " From: Burgas";
+        return "User: Plamen Kostov <br>" + " Age: 18 <br>" + " From: Burgas";
     }
 }
