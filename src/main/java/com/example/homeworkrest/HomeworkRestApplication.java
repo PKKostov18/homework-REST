3d07afb5ca98906cb4aa55e055831bbd1fd873b4
PKKostov18 <PKKostov18@codingburgas.bg>
2022-11-02 14:23:31 +0200

    Initial commit

---
+++ src/main/java/com/example/homeworkrest/HomeworkRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homeworkrest/HomeworkRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
